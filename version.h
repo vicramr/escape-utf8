f1e2ada1b93d1702a348146c36e4f8d4ce95b4cb
Vicram Rajagopalan <42707603+vicramr@users.noreply.github.com>
2019-09-17 15:17:05 -0500

    added a comment

--- version.h
+++ version.h
@@ -5,6 +5,9 @@
 /*
  * This file contains the ground-truth version numbers for escape-utf8.
  * Any file which needs access to the version numbers should #include this.
+ * 
+ * I think that putting the version number in a header file is standard practice.
+ * See this SO question: https://stackoverflow.com/q/27395120
  */
 
 #define MAJOR (0)
