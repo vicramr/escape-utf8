f2512b13f04a9c637433d9301ead12cce9b9c6cb
Vicram Rajagopalan <42707603+vicramr@users.noreply.github.com>
2021-02-01 05:32:42 +0000

    Wrote a fix for compiling winrun.c on appveyor This is a gross hack but it should work. Because we can't call cl.exe directly I added a CMakeLists file to compile it.

--- java/ci/win_ciscript.ps1
+++ java/ci/win_ciscript.ps1
@@ -27,9 +27,10 @@ if ((Get-FileHash -Algorithm SHA256 -Path .\commons-cli-1.4.jar).Hash -eq "FD3C7
 	echo "Now running javac"
 	javac -classpath commons-cli-1.4\commons-cli-1.4.jar -sourcepath .. -d . -Xlint ..\Escape.java
 	echo "Now compiling winrun"
-	& "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.28.29333\bin\Hostx64\x64\cl.exe" /permissive- /W4 /DNDEBUG ..\winrun.c
+	cmake ..\ci\ -DCMAKE_CONFIGURATION_TYPES="Release"
+	cmake --build . --config Release
 	echo "Now running tests"
-	C:\Python35-x64\python.exe ..\..\test\integration_tests.py ..\winrun.exe
+	C:\Python35-x64\python.exe ..\..\test\integration_tests.py .\Release\winrun.exe
 } else {
 	echo "Checksum did not match. Exiting now."
 	exit 1
