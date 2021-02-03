94982c414d91041d625b08a8ab44fd519a2e2ca0
Vicram Rajagopalan <42707603+vicramr@users.noreply.github.com>
2021-02-03 00:10:19 -0600

    Updated powershell script to explicitly set exit code

--- java/ci/win_ciscript.ps1
+++ java/ci/win_ciscript.ps1
@@ -1,19 +1,9 @@
 # This Powershell script is the equivalent of ciscript.sh, for Windows.
 # Like ciscript.sh, this assumes you are in the root dir of the repo.
 # This script is only intended for use on the Visual Studio 2019 image
-# in AppVeyor. As such, it has some hard-coded paths to executables.
+# in AppVeyor. As such, it has a hard-coded path to the Python executable.
 
 echo "Running Windows integration tests for Java"
-echo "java version:"
-java -version | %{ "$_" }
-echo "javac version:"
-javac -version | %{ "$_" }
-# Note: the -version commands print to stderr, which Powershell treats as an error.
-# The workaround is to convert the error objects to strings. See here:
-# https://stackoverflow.com/a/20950421
-# For some reason Powershell still throws an error, but it doesn't terminate the script
-# so we can just ignore those error messages.
-
 
 cd java
 md build
@@ -33,7 +23,11 @@ if ((Get-FileHash -Algorithm SHA256 -Path .\commons-cli-1.4.jar).Hash -eq "FD3C7
 	cmake --build . --config Release
 	echo "Now running tests"
 	C:\Python35-x64\python.exe ..\..\test\integration_tests.py .\Release\winrun.exe
+	exit $LastExitCode
 } else {
 	echo "Checksum did not match. Exiting now."
 	exit 1
 }
+
+# See this in-depth answer about returning exit codes:
+# https://stackoverflow.com/a/57468523
